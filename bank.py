1db97c6750cc16ce45e6a6027edc652a5106c5bf
sarahembarry <sarahembarry@gmail.com>
2024-02-02 16:59:09 +0000

    week 2 comment update

--- bank.py
+++ bank.py
@@ -8,14 +8,19 @@ amount2 =int(input('Enter amount 2 (in cent):'))
 
 
 ## Here I am creating the sum of these 2 values
+
 sum =(amount1+amount2)
+
 
 ## Here I a, creating the text response 
 ##and formatting the number value to 2 decimal places
+
+
 txt = 'The sum of these is € {:.2f}'
 
 ## Here i'm using the above to format what i want printed as the reposnse 
 ## and dividing the sum (cent value) by 100 to bring it to euros.
+
 print(txt.format(sum/100))
       
 
