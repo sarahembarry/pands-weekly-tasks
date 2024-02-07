485c68f6c442eb6155f88a173e2f9866e638e5b2
sarahembarry <sarahembarry@gmail.com>
2024-02-07 21:37:44 +0000

    d

--- accounts.py
+++ accounts.py
@@ -8,6 +8,6 @@ HiddenDigets= str('XXXXXX')
 OutPutAccountNo= HiddenDigets+SlicedAccountNo
 ##LengthOfaccountNo = len(AccountNo)
 
-##slicedaccountno = (accountno[6:10])
+
 
 print (F"number is {OutPutAccountNo}")
