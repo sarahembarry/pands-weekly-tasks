e84914e0c45d232634924d8f0b6a524bd3ed2016
sarahembarry <sarahembarry@gmail.com>
2024-02-29 21:08:11 +0000

    first commit

---
+++ squareroot.py
@@ -0,0 +1,45 @@
+
+#Resources:
+#https://thirumalai2024.medium.com/python-program-to-find-square-root-of-the-number-using-newtons-method-937c0e732756#:~:text=0.5*(approx%2Bn%2Fapprox,square%20root%20of%20the%20number.
+#https://hackernoon.com/calculating-the-square-root-of-a-number-using-the-newton-raphson-method-a-how-to-guide-yr4e32zo
+#https://runestone.academy/ns/books/published/thinkcspy/MoreAboutIteration/NewtonsMethod.html
+
+
+
+
+
+#Funtion using Newton's method to find the square root of a number:
+def sqrt(number):
+    #here we half the 'number' that has been passed into the function to get an approximation of the square root.
+    approx = 0.5 * number
+    #Here we're using Newton's method for the square root.
+    improved = 0.5 * (approx + number / approx)
+    #Here we're using a while loop : that if 'improved' is not equal to 'approx' update the value for 'approx' to the value for 'improved' 
+    #The while loop re-runs and Newton's method is executed until 'improved' is equal to or very close to 'aprox' 
+    #Then loop terminates 
+    while improved != approx:
+        approx = improved
+        improved = 0.5 * (approx + number / approx)
+    return approx
+
+
+
+#Main program
+
+#Asking the user to input a positive number.This can be a float. The variable 'number' will be the parameter/argument for the function.
+number = float(input("please enter a positive number:"))
+
+#Creating a while loop. If the number is less than or equal to 0 it will keep prompting for a positive number.
+while number <= 0:
+
+    print(f"That was not a positive number")
+    number= float(input("Please enter a positive number:"))
+
+
+
+# Once a value for 'number' has been entered it will be used as the argument for the function.
+# the output of this function will the the variable "result"    
+result = (sqrt(number))
+
+#Print out the response using the number that was origionally input and the result of the function.
+print ((f"The square root of {number} is {result}"))
